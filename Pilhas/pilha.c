f56821ed3cc4e9a3fe31a7fa3015411cb357a4eb
Guilmour Rossi <guilmour@alunos.utfpr.edu.br>
2016-03-10 11:19:59 -0300

    first upload - project lesson #1
    Aula de Pilhas, professor Hilton dia 03/03/2016

---
+++ Pilhas/pilha.c
@@ -0,0 +1,27 @@
+/*
+Est. Dados 1 (S73)- UTFPR - Pilhas
+Guilmour Rossi
+Marinho Welter
+Pedro Pereira
+Raissa Yukie Rodrigues
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct pilha
+{
+	int topo;
+	int elementos[10];
+};
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello world! \n");
+
+	struct pilha eq;
+
+
+	return 0;
+}
+
