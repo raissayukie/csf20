8a96095f480764bd319abf06cb4d5d69703988b6
Guilmour Rossi <guilmour@alunos.utfpr.edu.br>
2016-03-10 14:40:05 -0300

    recebendo do usuário pilha.c

--- Pilhas/pilha.c
+++ Pilhas/pilha.c
@@ -9,19 +9,42 @@ Raissa Yukie Rodrigues
 #include <stdio.h>
 #include <stdlib.h>
 
+/*inicia pilha*/
 struct pilha
 {
 	int topo;
-	int elementos[10];
+	int elementos[100];
 };
 
+
+/* main */
 int main(int argc, char const *argv[])
 {
-	printf("Hello world! \n");
+	/* Input do Usuário com sua equação */
+	char input[20]; 	
+	
+	printf("Entre sua equação: \n");
+	
+	/*Recebe do teclado do Usuário*/
+	gets(input);
 
-	struct pilha eq;
+	struct pilha calc;
+	calc.topo = -1;
+
+	//testaSeVazia(ptpilha);
 
 
 	return 0;
 }
+
+/*bool testaSeVazia(calc){
+
+	if(ptpilha->topo == -1){
+		printf("A pilha esta vazia.\n");
+		return 1;
+	}else{
+		printf("A pilha nao estah vazia.\n");
+		return 0;
+	}	
+}*/
 
