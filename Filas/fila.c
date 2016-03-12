e24465bdd6ae8cfc9ab58cbd3feca291ef104352
Guilmour <guilmour@alunos.utfpr.edu.br>
2016-03-12 10:48:23 -0300

    estrtura do doc c

--- Filas/fila.c
+++ Filas/fila.c
@@ -5,3 +5,13 @@ Marinho Welter
 Pedro Pereira
 Raissa Yukie Rodrigues
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int int main(int argc, char const *argv[])
+{
+	setlocale(LC_ALL, "Portuguese");
+	return 0;
+}
